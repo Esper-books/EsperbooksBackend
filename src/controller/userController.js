03192b0f1d828be3ee27211614a0fc5c35ca1963
unknown <samuel.sawyerr@interswitchng.com>
2023-06-05 05:11:36 +0100

    optimize and added reset password

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -7,7 +7,11 @@ var companyRepository = require("../repo/companyRepo");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const secretKey = "secret esperbook";
-var mailingService = require('../service/mailing_service')
+var mailingService = require("../service/mailing_service");
+var util = require("../utils/util");
+var resetpasswordRepo = require("../repo/resetPasswordTokenRepo");
+const { error } = require("console");
+const { encode } = require("querystring");
 
 var reqBody;
 
@@ -25,85 +29,179 @@ router.post("", (req, res) => {
     });
   }
 
-  reqBody.password  = encryptPassword(reqBody.password, salt);
-  companyRepository
-    .fetchCompanyByToken(companyToken, (presp) => {
-      if (presp == null) {
-        return res.status(404).json({
-          responseCode: 404,
-          responseMessage: "Token does not exist!",
-        });
-      }
+  reqBody.password = encrypt(reqBody.password, secretKey);
+  companyRepository.fetchCompanyByToken(companyToken, (presp) => {
+    if (presp == null) {
+      return res.status(404).json({
+        responseCode: 404,
+        responseMessage: "Token does not exist!",
+      });
+    }
 
-      const reqBody2 = new requestObject(
-        reqBody.firstName,
-        reqBody.lastName,
-        reqBody.dateOfBirth,
-        reqBody.phoneNumber,
-        reqBody.gender,
-        reqBody.country,
-        reqBody.emailAddress,
-        reqBody.password,
-        reqBody.confirmPassword,
-        presp.companyToken
-      );
-      userRepository.createUser(reqBody2)
-      .then(
-        (presp) => {
-          mailingService.sendCreateMailNotification(presp);
+    const reqBody2 = new requestObject(
+      reqBody.firstName,
+      reqBody.lastName,
+      reqBody.dateOfBirth,
+      reqBody.phoneNumber,
+      reqBody.gender,
+      reqBody.country,
+      reqBody.emailAddress,
+      reqBody.password,
+      reqBody.confirmPassword,
+      presp.companyToken
+    );
+    userRepository
+      .createUser(reqBody2)
+      .then((presp) => {
+        mailingService.sendCreateMailNotification(presp);
+        return res.status(200).json({
+          responseCode: 200,
+          responseMessage: "User Created Successfully!",
+        });
+      })
+      .catch((error) => {
+        if (error.name === "SequelizeUniqueConstraintError") {
+          // Handle the duplicate error
+          const value = error.errors[0].value;
+          if (error)
+            return res
+              .status(400)
+              .json({ responseMessage: `${value} already exist.` });
+        } else {
+          // Handle other errors
+          console.error(error);
+        }
+        return res
+          .status(500)
+          .json({ responseCode: 500, responseMessage: "Server Error" });
+      });
+  });
+});
+
+router.post("/send/resetpassword/link", (req, res) => {
+  userReq = req.body;
+
+  const token = jwt.sign({ email: userReq.email }, secretKey, {
+    expiresIn: "1h",
+  });
+
+  const reqbody = { email: userReq.email, token: token };
+
+  userRepository.fetchUserByEmail(reqbody.email , ((data) => {
+    if (data == null) {
+      return res.status(404).json({
+        responseCode: 404,
+        responseMessage: "User does not exist",
+      });
+    }
+    resetpasswordRepo
+    .createResetPassword(reqbody)
+    .then((presp) => {
+      mailingService.sendResetPasswordNotification(reqbody);
+      return res.status(200).json({
+        responseCode: 200,
+        responseMessage: "Reset link has been sent to email if it exist.",
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ responseCode: 500, responseMessage: "Server Error" });
+    });
+  }
+  )).catch(error => {
+    console.log(error);
+  });
+});
+
+router.post("/resetpassword", (req, res) => {
+  reqBody = req.body;
+  const { token } = req.query;
+
+  resetpasswordRepo
+    .fetchResetPByToken(token, (presp) => {
+      if (presp == null)
+        if (isTokenExpired(token)) {
           return res
-            .status(200)
-            .json({
-              responseCode: 200,
-              responseMessage: "User Created Successfully!",
+            .status(400)
+            .json({ responseCode: 400, responseMessage: "Token has expired" });
+        }
+
+      reqBody.newpassword = encrypt(reqBody.newpassword, secretKey);
+      userRepository
+        .updatePassword({
+          email: presp.email,
+          newpassword: reqBody.newpassword,
+        })
+        .then((data) => {
+          if (data == undefined) {
+            return res.status(500).json({
+              responseCode: 500,
+              responseMessage: "Failed",
             });
-        }) .catch((error) => {
-          if (error.name === 'SequelizeUniqueConstraintError') {
-            // Handle the duplicate error
-            const value = error.errors[0].value;
-            if (error) return res.status(400).json(    
-              {responseMessage:`${value} already exist.`});
-          } else {
-            // Handle other errors
-             console.error(error);
           }
-          return res
-            .status(500)
-            .json({ responseCode: 500, responseMessage: "Server Error" });
+          return res.status(200).json({
+            responseCode: 200,
+            responseMessage: "Password Changed Successfully!",
+          });
+        })
+        .catch((error) => {
+          return res.status(500).json({
+            responseCode: 500,
+            responseMessage: "Failed",
+          });
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      console.error(error);
+      return res
+        .status(500)
+        .json({ responseCode: 500, responseMessage: "Server Error" });
     });
 });
 
-});
-
-function requestObject( firstName       
-  ,lastName        
-  ,dateOfBirth     
-  ,phoneNumber     
-  ,gender          
-  ,country         
-  ,emailAddress     
-  ,password        
-  ,confirmPassword 
-,companyToken){
-this.firstName        = firstName;
-this.lastName         = lastName   ;
-this.dateOfBirth      = dateOfBirth;
-this.phoneNumber      = phoneNumber;
-this.gender           = gender     ;
-this.country          = country    ;
-this.emailAddress      = emailAddress;
-this.password         = password   ;
-this.confirmPassword  = confirmPassword;
-this.companyToken  = companyToken  ;
+function requestObject(
+  firstName,
+  lastName,
+  dateOfBirth,
+  phoneNumber,
+  gender,
+  country,
+  emailAddress,
+  password,
+  confirmPassword,
+  companyToken
+) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dateOfBirth = dateOfBirth;
+  this.phoneNumber = phoneNumber;
+  this.gender = gender;
+  this.country = country;
+  this.emailAddress = emailAddress;
+  this.password = password;
+  this.confirmPassword = confirmPassword;
+  this.companyToken = companyToken;
 }
 
-const salt = crypto.randomBytes(16);
-function encryptPassword(password, salt) {
-  const key = crypto.scryptSync(password, salt, 32);
-  const cipher = crypto.createCipheriv('aes-256-cbc', key, salt);
-  let encrypted = cipher.update(password, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
+function encrypt(text, password) {
+  const cipher = crypto.createCipher("aes-256-cbc", password);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
   return encrypted;
+}
+
+function isTokenExpired(token) {
+  try {
+    const decodedToken = jwt.verify(token, secretKey);
+    const currentTime = Date.now() / 1000; // Convert to seconds
+    return decodedToken.exp < currentTime;
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return true; // Consider token as expired in case of error
+  }
 }
 
 module.exports = router;
--- src/service/mailing_service.js
+++ src/service/mailing_service.js
@@ -1,39 +1,57 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
-
-
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: process.env.EMAIL_TRANSPORT_USER, 
-        pass: process.env.EMAIL_TRANSPORT_PASS 
-    }
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_TRANSPORT_USER,
+    pass: process.env.EMAIL_TRANSPORT_PASS,
+  },
 });
-
 
 // send mail with defined transport object
 
-function sendCreateMailNotification(pres){
-    console.log(pres.emailAddress);
-    let mailOptions = {
-        from: process.env.EMAIL_TRANSPORT_FROM, 
-        to: pres.emailAddress, 
-        subject: process.env.EMAIL_COMPANY_CREATE_SUBJECT, 
-        text: process.env.BASE_URI+process.env.EMAIL_COMPANY_SIGNUP_USER_URL+pres.companyToken
-        // html: '<b>baseUrl+token</b>'
-    };
-transporter.sendMail(mailOptions, (error, info) => {
-    
+function sendCreateMailNotification(pres) {
+  let mailOptions = {
+    from: process.env.EMAIL_TRANSPORT_FROM,
+    to: pres.emailAddress,
+    subject: process.env.EMAIL_COMPANY_CREATE_SUBJECT,
+    text:
+      process.env.BASE_URI +
+      process.env.EMAIL_COMPANY_SIGNUP_USER_URL +
+      pres.companyToken,
+    // html: '<b>baseUrl+token</b>'
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-        return console.log(error);
+      return console.log(error);
     }
-    console.log('Message sent: %s', info.messageId);
-});
+    console.log("Message sent: %s", info.messageId);
+  });
 }
- 
+
+
+function sendResetPasswordNotification(pres) {
+    let mailOptions = {
+      from: process.env.EMAIL_TRANSPORT_FROM,
+      to: pres.email,
+      subject: "RESET PASSWORD NOTIFICATION",
+      text:
+        process.env.BASE_URI +
+        "/user/resetpassword?token=" +
+        pres.token,
+      // html: '<b>baseUrl+token</b>'
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+    });
+  }
+
 module.exports = {
-    sendCreateMailNotification : sendCreateMailNotification
-   }
+  sendCreateMailNotification,sendResetPasswordNotification
+};
